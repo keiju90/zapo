1319fe801d6e8a9d132e569fc3a14e6207974678
Sofia Junell <keijudesign@gmail.com>
2020-01-10 10:43:44 +0100

    moved the div outside function

--- displayImg.js
+++ displayImg.js
@@ -1,10 +1,10 @@
 //display image
+var imgContainer = document.createElement('div'); // create div
+imgContainer.id="imgCont"; // div id imaCont
 
 function buildImage(){ //create image function
    var img = document.createElement('img') //create image element
    img.src = imgObj[index]; // image source from array above
-   var imgContainer = document.createElement('div'); // create div
-   imgContainer.id="imgCont"; // div id imaCont
    imgContainer.appendChild(img); // append img to div container
    document.body.appendChild(imgContainer); // append div to body
 };
